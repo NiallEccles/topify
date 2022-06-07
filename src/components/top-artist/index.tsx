10773928041749cd4a52c84456702454b0598e38
Niall Eccles <nialle@rothco.ie>
2022-06-07 21:52:16 +0100

    Updated top artist tile with text and better overlay.

--- src/components/top-artist/index.tsx
+++ src/components/top-artist/index.tsx
@@ -13,6 +13,10 @@ function TopArtist(props: props) {
   return (
     <div className="top-artist" style={{ backgroundImage: `url(${props.imgUrl})` }}>
       {props.children}
+      <div className="z-correction">
+        <h3 className="top-artist-title">Top Artist</h3>
+        <h2 className="top-artist-name">{props.artistName}</h2>
+      </div>
     </div>
   );
 }
