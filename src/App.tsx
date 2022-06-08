3a9907475bf991dda3c475974fbf1dc22bd1375f
Niall Eccles <nialle@rothco.ie>
2022-06-08 16:42:47 +0100

    Start work on completely separating components and interfaces.

--- src/App.tsx
+++ src/App.tsx
@@ -7,51 +7,63 @@ import Artwork from "./components/artwork";
 import TopArtist from "./components/top-artist";
 import TopGenre from "./components/top-genres";
 import Chip from "./components/chip";
+import TopTrackss from "./components/top-tracks";
+import { TopTracks } from "./utils/toptracks";
 
 function App() {
   let genres: string[] = [];
-  data.topArtists.items.forEach((item)=>{
+  data.topArtists.items.forEach((item) => {
     genres.push(...item.genres);
   });
 
   interface Counts {
-    [key: string]: number
+    [key: string]: number;
   }
   let counts: Counts = {};
-  genres.forEach(function (genre) { counts[genre] = (counts[genre] || 0) + 1; });
+  genres.forEach(function (genre) {
+    counts[genre] = (counts[genre] || 0) + 1;
+  });
 
-  let topGenres = [];
-
-  Object.keys(counts).forEach(key => {
+  Object.keys(counts).forEach((key) => {
     if (counts[key] < 3) delete counts[key];
   });
+  console.log(data.topTracks)
 
   return (
     <main>
+      <h1 style={{ gridArea: "site-title" }}>Topify</h1>
       <div className="container">
-      <h1 style={{gridArea: 'site-title'}}>Topify</h1>
         <Card style={{ gridArea: "name" }}>
           <DisplayName displayName={data.me.display_name} />
           <ShareButton text="" alignment={"bottom-right"} />
         </Card>
-        <div style={{display: 'flex', flexDirection: 'column', gridArea: "top-genre"}}>
+        <div
+          style={{
+            display: "flex",
+            flexDirection: "column",
+            gridArea: "top-genre",
+          }}
+        >
           <h2 className="section-heading">Top Genres</h2>
           <Card style={{ gridArea: "top-genre" }}>
-            {
-              Object.keys(counts).map(genre=>{
-                return <Chip text={genre}></Chip>
-              })
-            }
-            {/* <TopGenre genres={data.topArtists.items} /> */}
+            {Object.keys(counts).map((genre) => {
+              return <Chip text={genre}></Chip>;
+            })}
           </Card>
         </div>
-        <div style={{display: 'flex', flexDirection: 'column', gridArea: "stat"}}>
+        <div
+          style={{
+            display: "flex",
+            flexDirection: "column",
+            gridArea: "top-artist",
+          }}
+        >
           <h2 className="section-heading">Top Artist</h2>
           <Card
             style={{
               padding: "0",
-              height: '100%',
-              flex: 1
+              height: "100%",
+              flex: 1,
             }}
           >
             <TopArtist
@@ -61,35 +73,8 @@ function App() {
           </Card>
         </div>
         <div style={{ gridArea: "top-song" }}>
-          <h2 className="section-heading">Top Tracks</h2>
-          {data.topTracks.items.map((track, index) => {
-            if (index < 6) {
-              return (
-                <Card style={{ padding: "0", margin: "0 0 1em 0" }}>
-                  <Artwork
-                    key={index}
-                    songName={track.name}
-                    artistName={track.album.artists}
-                    imgUrl={track.album.images[0].url}
-                  />
-                </Card>
-              );
-            } else {
-              return <></>;
-            }
-          })}
+          <TopTrackss data={(data.topTracks as TopTracks)}/>
         </div>
-        {/* <Card
-          style={{
-            padding: "0",
-            gridArea: "top-genre",
-          }}
-        >
-          <TopArtist
-            artistName={data.topArtists.items[0].name}
-            imgUrl={data.topArtists.items[0].images[0].url}
-          />
-        </Card> */}
       </div>
     </main>
   );
---
+++ src/components/top-tracks/index.tsx
@@ -0,0 +1,38 @@
+import React from "react";
+import "./style.css";
+import CSS from "csstype";
+import Card from "../card";
+import Artwork from "../artwork";
+import { TopTracks } from "../../utils/toptracks";
+
+interface props {
+  style?: CSS.Properties;
+  children?: JSX.Element | JSX.Element[];
+  data: TopTracks;
+}
+
+function TopTrackss(props: props) {
+  return (
+    <div>
+      <h2 className="section-heading">Top Tracks</h2>
+      {props.data.items.map((track, index) => {
+        if (index < 6) {
+          return (
+            <Card style={{ padding: "0", margin: "0 0 1em 0" }}>
+              <Artwork
+                key={index}
+                songName={track.name}
+                artistName={track.album.artists}
+                imgUrl={track.album.images[0].url}
+              />
+            </Card>
+          );
+        } else {
+          return <></>;
+        }
+      })}
+    </div>
+  );
+}
+
+export default TopTrackss;
