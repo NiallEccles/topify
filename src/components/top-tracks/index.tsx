883593e257166efb950bc12780f47436846fbad9
Niall Eccles <nialle@rothco.ie>
2022-06-08 17:21:22 +0100

    Created container around top track cards.

--- src/components/top-tracks/index.tsx
+++ src/components/top-tracks/index.tsx
@@ -15,21 +15,23 @@ function TopTrackss(props: props) {
   return (
     <div style={{ gridArea: "top-song" }}>
       <h2 className="section-heading">Top Tracks</h2>
-      {props.data.items.map((track, index) => {
-        if (index < 6) {
-          return (
-            <Card style={{ padding: "0", margin: "0 0 1em 0" }} key={index}>
-              <Artwork
-                songName={track.name}
-                artistName={track.album.artists}
-                imgUrl={track.album.images[0].url}
-              />
-            </Card>
-          );
-        } else {
-          return <></>;
-        }
-      })}
+      <div className="tracks">
+        {props.data.items.map((track, index) => {
+          if (index < 6) {
+            return (
+              <Card style={{ padding: "0", margin: "0 0 1em 0" }} key={index}>
+                <Artwork
+                  songName={track.name}
+                  artistName={track.album.artists}
+                  imgUrl={track.album.images[0].url}
+                />
+              </Card>
+            );
+          } else {
+            return <></>;
+          }
+        })}
+      </div>
     </div>
   );
 }
